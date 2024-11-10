66f6db7a2914027f89cc503fed701f7b001ba4a6
Mekmun Sopheaktra <m-sopheaktra@beniten.com>
2024-11-10 11:04:44 +0700

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Loading />
+    <PageHeader />
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
+<script setup>
+import { useAuthStore } from '~/store/auth'
+const { isLoggedIn } = useAuthenticate()
+if (isLoggedIn.value) {
+  await useAuthStore().getMe()
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container-fluid home-content">
+    <button @click="navigateTo('/profile')">Go to Dashboard</button>
+    <site-title />
+  </div>
+</template>
+<script setup>
+useSeoMeta({ title: 'ITE | Home' })
+// navigateTo('/dashboard')
+</script>
