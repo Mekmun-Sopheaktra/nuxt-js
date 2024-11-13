662cd73a653c8897f7332d9296c8377931aa85f8
Mekmun Sopheaktra <m-sopheaktra@beniten.com>
2024-11-13 11:32:03 +0700

    Add SSR

--- pages/index.vue
+++ pages/index.vue
@@ -3,11 +3,57 @@
     <button @click="navigateTo('/profile')">Go to Dashboard</button>
     <site-title />
     <el-button type="primary" @click="ElMessage('hello')">Button</el-button>
+    <el-carousel :interval="4000" type="card" height="200px">
+    <el-carousel-item v-for="item in 6" :key="item">
+      <h3 text="2xl" justify="center">{{ item }}</h3>
+    </el-carousel-item>
+  </el-carousel>
+    <div>adwdasdwqdsd</div>
+    <div>{{a}}</div>
+  </div>
+
+  <div class="block text-center">
+    <span class="demonstration">
+      Switch when indicator is hovered (default)
+    </span>
+    <el-carousel height="150px">
+      <el-carousel-item v-for="item in 4" :key="item">
+        <h3 class="small justify-center" text="2xl">{{ item }}</h3>
+      </el-carousel-item>
+    </el-carousel>
+  </div>
+  <div class="block text-center" m="t-4">
+    <span class="demonstration">Switch when indicator is clicked</span>
+    <el-carousel trigger="click" height="150px">
+      <el-carousel-item v-for="item in 4" :key="item">
+        <h3 class="small justify-center" text="2xl">{{ item }}</h3>
+      </el-carousel-item>
+    </el-carousel>
   </div>
 </template>
+
 <script setup>
 import { ElMessage } from "element-plus";
 
 useSeoMeta({ title: "ITE | Home" });
 // navigateTo('/dashboard')
+const a = 1;
 </script>
+
+<style scoped>
+.el-carousel__item h3 {
+  color: #475669;
+  opacity: 0.75;
+  line-height: 200px;
+  margin: 0;
+  text-align: center;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
+</style>
